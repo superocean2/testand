27a2598f2e84962f55888a48f926fd6d1da0e42a
trinhnghia <superocean2@gmail.com>
2015-06-17 23:03:37 +0700

    update

---
+++ Game/android/src/samsam/com/android/TestDrop.java
@@ -0,0 +1,43 @@
+package samsam.com.android;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
+
+/**
+ * Created by Sam Sam on 6/17/2015.
+ */
+public class TestDrop implements ApplicationListener {
+    private Texture bucketImage;
+    private Rectangle bucket;
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
--- GameRabbit/core/src/samsam/nghia/RainDrop.java
+++ GameRabbit/core/src/samsam/nghia/RainDrop.java
@@ -1,11 +1,17 @@
 package samsam.nghia;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+
+import java.util.ArrayList;
 
 /**
  * Created by NghiaTrinh on 6/17/2015.
@@ -15,54 +21,68 @@ public class RainDrop extends ApplicationAdapter {
     SpriteBatch batch;
     Texture background;
     OrthographicCamera camera;
-    Texture bucket;
-    float bucketPosition_X;
-    float bucketPosition_Y=10;
-    Texture rainDrop;
+    Texture bucketImage;
+    Texture rainDropImage;
+    Rectangle bucketRec;
+    ArrayList<Rectangle> rainDrops;
 
     @Override
     public void create() {
         batch= new SpriteBatch();
         background = new Texture("background.png");
-        bucket = new Texture("bucket.png");
-        bucketPosition_X=800/2-bucket.getWidth()/2;
-        rainDrop = new Texture("droplet.png");
+        bucketImage = new Texture("bucket.png");
+        bucketRec=new Rectangle();
+        bucketRec.x=800/2-bucketImage.getWidth()/2;
+        bucketRec.y=10;
+        bucketRec.width=64;
+        bucketRec.height=64;
+        rainDropImage = new Texture("droplet.png");
         camera=new OrthographicCamera();
         camera.setToOrtho(false,800,480);
+        rainDrops=new ArrayList<Rectangle>();
     }
 
     @Override
     public void render() {
         Gdx.gl.glClearColor(1,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
         camera.update();
         batch.setProjectionMatrix(camera.combined);
         batch.begin();
         batch.draw(background, 0, 0);
-        batch.draw(bucket,bucketPosition_X,bucketPosition_Y);
+        batch.draw(bucketImage,bucketRec.x,bucketRec.y);
         batch.end();
     }
 
     @Override
     public void resize(int width, int height) {
-        super.resize(width, height);
+
     }
 
     @Override
     public void pause() {
-        super.pause();
+
     }
 
     @Override
     public void resume() {
-        super.resume();
+
     }
 
     @Override
     public void dispose() {
         background.dispose();
         batch.dispose();
-        bucket.dispose();
+        bucketImage.dispose();
+    }
+
+    private void initRainDrop()
+    {
+        Rectangle raindrop = new Rectangle();
+        raindrop.x = MathUtils.random(0,800-64);
+        raindrop.y=480;
+        raindrop.width=64;
+        raindrop.height=64;
+        rainDrops.add(raindrop);
     }
 }
