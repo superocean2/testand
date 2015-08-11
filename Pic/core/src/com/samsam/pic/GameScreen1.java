47bd73bb7e9f084695bc48ea9477ba39c4c0a436
Nghia <superocean2@gmail.com>
2015-08-11 16:47:40 +0700

    init new Pic

---
+++ Pic/core/src/com/samsam/pic/GameScreen1.java
@@ -0,0 +1,64 @@
+package com.samsam.pic;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+
+/**
+ * Created by NghiaTrinh on 8/11/2015.
+ */
+public class GameScreen1 implements Screen {
+
+    final PicGame game;
+    OrthographicCamera camera;
+
+    public GameScreen1(PicGame game) {
+        this.game = game;
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 1280, 800);
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(255, 255, 255, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        camera.update();
+        game.batch.setProjectionMatrix(camera.combined);
+
+        game.batch.begin();
+        game.batch.draw(game.pic1,0,0);
+        game.batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
