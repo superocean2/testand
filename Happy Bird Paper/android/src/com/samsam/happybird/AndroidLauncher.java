3648708870de6f4911804fa4dfa50717011094f1
Nghia <superocean2@gmail.com>
2016-04-05 16:12:12 +0700

    add happy bird

---
+++ Happy Bird Paper/android/src/com/samsam/happybird/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.samsam.happybird;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.samsam.happybird.HappyBird;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new HappyBird(), config);
+	}
+}
